6a0472278885c7f0f487559f3999c4ec7da12525
Alexey Malets <alex@expil.pro>
2016-08-15 12:24:02 +0300

    Update 1.8.1(Stable release)
    Fixed some bugs
    Added ability to select release

--- nginx_install_ru.sh
+++ nginx_install_ru.sh
@@ -33,6 +33,7 @@ loc_srv_admin='Бесплатная помощь с администрирова
 loc_myhosti='В платный отдел администрирования'
 loc_error='Ошибка'
 loc_thanks_soft='Спасибо за использование данного ПО!'
+loc_ver_repo='Какой вариант Вы бы хотели установить? ?'
 
 
 clear
@@ -74,83 +75,92 @@ elif [ "$(cat /etc/*-release | grep 'CentOS Linux release 7')" ] || [ "$(cat /et
 	dist=centos
 	osv=7
 else
-	echo "${BOLD}${loc_none_script}!${NORMAL}"
+	echo "${loc_none_script}!"
 	exit 0
 fi
-
-echo "${BOLD}${BGRED}${loc_your_os} ${dist} ${osv}${NORMAL}"
-
+if [ "${dist}" = "debian" ] || [ "${dist}" = "ubuntu" ]; then
+	echo "${loc_your_os} ${dist} ${osv}"
+else
+	echo "${loc_your_os} ${dist} ${osv}"	
+fi
 checknginxstatus=0
 
 if [ "${dist}" = "debian" ] || [ "${dist}" = "ubuntu" ]; then
-	BOLD='\033[1m'
-	RED='\033[0;31m'
-	GREEN='\033[0;32m'
-	BGRED='\033[41m'
-	NORMAL='\033[0m'
 	if [ -z "$(grep -rn "nginx.org" /etc/apt/)" ]; then
-		echo "${GREEN}${loc_go_to_dir} /etc/apt/sources.list.d/${NORMAL}"
-		cd /etc/apt/sources.list.d/
-
-		echo "${GREEN}${loc_add_rep}${NORMAL}"
-		touch nginx.list
-		echo "deb http://nginx.org/packages/mainline/${dist}/ ${osv} nginx" | tee -a /etc/apt/sources.list.d/nginx.list
-		echo "deb-src http://nginx.org/packages/mainline/${dist}/ ${osv} nginx" | tee -a /etc/apt/sources.list.d/nginx.list
+		echo "${loc_ver_repo}"
+		echo "1) Mainline"
+		echo "2) Stable"
+		read -p "${loc_choose}: " ver_repo
+		if [ $ver_repo -eq 1 ]; then
+			echo "${loc_go_to_dir} /etc/apt/sources.list.d/"
+			cd /etc/apt/sources.list.d/
+			echo "${loc_add_rep}"
+			touch nginx.list
+			echo "deb http://nginx.org/packages/mainline/${dist}/ ${osv} nginx" | tee -a /etc/apt/sources.list.d/nginx.list
+			echo "deb-src http://nginx.org/packages/mainline/${dist}/ ${osv} nginx" | tee -a /etc/apt/sources.list.d/nginx.list
+		else
+			echo "${loc_go_to_dir} /etc/apt/sources.list.d/"
+			cd /etc/apt/sources.list.d/
+			echo "${loc_add_rep}"
+			touch nginx.list
+			echo "deb http://nginx.org/packages/${dist}/ ${osv} nginx" | tee -a /etc/apt/sources.list.d/nginx.list
+			echo "deb-src http://nginx.org/packages/${dist}/ ${osv} nginx" | tee -a /etc/apt/sources.list.d/nginx.list
+		fi
 		
 		cd
-		echo "${GREEN}${loc_check_key_sys}${NORMAL}"
+		echo "${loc_check_key_sys}"
 		if [ -z "$(apt-key list | grep nginx)" ]; then
-			echo "${GREEN}${loc_add_key}${NORMAL}"
+			echo "${loc_add_key}"
 			wget http://nginx.org/keys/nginx_signing.key
 			apt-key add nginx_signing.key
 		fi
-		echo "${GREEN}${loc_check_nginx}${NORMAL}"
+		echo "${loc_check_nginx}"
 
 		if [ -z "$(dpkg -l|grep nginx)" ]; then
-			echo "${BOLD}${GREEN}${loc_install_nginx}${NORMAL}"
+			echo "${loc_install_nginx}"
 			apt-get update && apt-get -y install nginx
 		else
-			echo "${BOLD}${GREEN}${loc_nginx_al_in}${NORMAL}"
-			echo "${GREEN}${loc_nginx_upgrade}${NORMAL}"
+			echo "${loc_nginx_al_in}"
+			echo "${loc_nginx_upgrade}"
 			apt-get update && apt-get -y upgrade nginx
 		fi
 	else
 		if [ -z "$(grep -rn "nginx.org/packages/mainline" /etc/apt/)" ]; then
-			echo "${BOLD}${RED}${loc_nginx_repo_find}${NORMAL}"
-			echo "${BOLD}${GREEN}${loc_install_last_v_nginx}${NORMAL}"
+			echo "${loc_nginx_repo_find}"
+			echo "${loc_install_last_v_nginx}"
 			echo "1) ${loc_yes}"
 			echo "2) ${loc_no}"
 			read -p "${loc_choose}: " installnginx
 			if [ $installnginx -eq 1 ]; then
-				echo "${GREEN}${loc_go_to_dir} /etc/apt/sources.list.d/${NORMAL}"
+				echo "${loc_go_to_dir} /etc/apt/sources.list.d/"
 				cd /etc/apt/sources.list.d/
-				echo "${GREEN}${loc_remove_repo}${NORMAL}"
+				echo "${loc_remove_repo}"
 				for listnginx in $(grep -rl "nginx.org" /etc/apt/)
 				do
 					sed -i '/nginx.org/d' $listnginx
 				done
-				echo "${GREEN}${loc_mainline_repo}${NORMAL}"
+				echo "${loc_mainline_repo}"
 				touch nginx.list
 				echo "deb http://nginx.org/packages/mainline/${dist}/ ${osv} nginx" | tee -a /etc/apt/sources.list.d/nginx.list
 				echo "deb-src http://nginx.org/packages/mainline/${dist}/ ${osv} nginx" | tee -a /etc/apt/sources.list.d/nginx.list
 				if [ -z "$(dpkg -l|grep nginx)" ]; then
-					echo "${GREEN}${loc_install_last_ver}${NORMAL}"
+					echo "${loc_install_last_ver}"
 					apt-get update && apt-get -y install nginx
 				else
-					echo "${GREEN}${loc_upgrade_last_ver}${NORMAL}"
+					echo "${loc_upgrade_last_ver}"
 					apt-get update && apt-get -y upgrade nginx
 				fi
 			else
-				echo "${BOLD}${RED}${loc_no_install_last_ver}${NORMAL}"
+				echo "${loc_no_install_last_ver}"
 				exit 0
 			fi
 		else
-			echo "${BOLD}${RED}${loc_setup_last_version}${NORMAL}"
+			echo "${loc_setup_last_version}"
 			if [ -z "$(dpkg -l|grep nginx)" ]; then
-				echo "${GREEN}${loc_install_last_ver}${NORMAL}"
+				echo "${loc_install_last_ver}"
 				apt-get update && apt-get -y install nginx
 			else
-				echo "${GREEN}${loc_upgrade_last_ver}${NORMAL}"
+				echo "${loc_upgrade_last_ver}"
 				apt-get update && apt-get -y upgrade nginx
 			fi
 		fi
@@ -158,74 +168,90 @@ if [ "${dist}" = "debian" ] || [ "${dist}" = "ubuntu" ]; then
 	checknginxstatus=1
 else
 	if [ -z "$(grep -rn "nginx.org" /etc/yum.repos.d/)" ]; then
-		echo "${GREEN}${loc_go_to_dir} /etc/yum.repos.d/${NORMAL}"
-		cd /etc/yum.repos.d/
-
-		echo "${GREEN}${loc_add_rep}${NORMAL}"
-		touch nginx.repo	
-		echo "[nginx]" | tee -a /etc/yum.repos.d/nginx.repo
-		echo "name=nginx repo" | tee -a /etc/yum.repos.d/nginx.repo
-		echo "baseurl=http://nginx.org/packages/mainline/${dist}/${osv}/"'$basearch/' | tee -a /etc/yum.repos.d/nginx.repo
-		echo "gpgcheck=0" | tee -a /etc/yum.repos.d/nginx.repo
-		echo "enabled=1" | tee -a /etc/yum.repos.d/nginx.repo
+		echo "${loc_ver_repo}"
+		echo "1) Mainline"
+		echo "2) Stable"
+		read -p "${loc_choose}: " ver_repo
+		if [ $ver_repo -eq 1 ]; then
+			echo "${loc_go_to_dir} /etc/yum.repos.d/"
+			cd /etc/yum.repos.d/
+			echo "${loc_add_rep}"
+			touch nginx.repo			
+			echo "[nginx]" | tee -a /etc/yum.repos.d/nginx.repo
+			echo "name=nginx repo" | tee -a /etc/yum.repos.d/nginx.repo
+			echo "baseurl=http://nginx.org/packages/mainline/${dist}/${osv}/"'$basearch/' | tee -a /etc/yum.repos.d/nginx.repo
+			echo "gpgcheck=0" | tee -a /etc/yum.repos.d/nginx.repo
+			echo "enabled=1" | tee -a /etc/yum.repos.d/nginx.repo
+		else
+			echo "${loc_go_to_dir} /etc/yum.repos.d/"
+			cd /etc/yum.repos.d/
+			echo "${loc_add_rep}"
+			touch nginx.repo
+			echo "[nginx]" | tee -a /etc/yum.repos.d/nginx.repo
+			echo "name=nginx repo" | tee -a /etc/yum.repos.d/nginx.repo
+			echo "baseurl=http://nginx.org/packages/${dist}/${osv}/"'$basearch/' | tee -a /etc/yum.repos.d/nginx.repo
+			echo "gpgcheck=0" | tee -a /etc/yum.repos.d/nginx.repo
+			echo "enabled=1" | tee -a /etc/yum.repos.d/nginx.repo
+		fi	
+		
 		cd
-		echo "${GREEN}${loc_check_key_sys}${NORMAL}"
+		echo "${loc_check_key_sys}"
 		if [ -z "$(rpm -q gpg-pubkey --qf '%{name}-%{version}-%{release} --> %{summary}\n' | grep nginx)" ]; then
-			echo "${GREEN}${loc_add_key}${NORMAL}"
+			echo "${loc_add_key}"
 			wget http://nginx.org/keys/nginx_signing.key
 			rpm --import nginx_signing.key
 		fi
 		
-		echo "${GREEN}${loc_check_nginx}${NORMAL}"
+		echo "${loc_check_nginx}"
 
 		if [ -z "$(yum list installed|grep nginx)" ]; then
-			echo "${BOLD}${GREEN}${loc_install_nginx}${NORMAL}"
+			echo "${loc_install_nginx}"
 			yum -y update && yum -y install nginx
 		else
-			echo "${BOLD}${GREEN}${loc_nginx_al_in}${NORMAL}"
-			echo "${GREEN}${loc_nginx_upgrade}${NORMAL}"
+			echo "${loc_nginx_al_in}"
+			echo "${loc_nginx_upgrade}"
 			yum update -y nginx
 		fi
 	else
 		if [ -z "$(grep -rn "nginx.org/packages/mainline" /etc/yum.repos.d/)" ]; then
-			echo "${BOLD}${RED}${loc_nginx_repo_find}${NORMAL}"
-			echo "${BOLD}${GREEN}${loc_install_last_v_nginx}${NORMAL}"
+			echo "${loc_nginx_repo_find}"
+			echo "${loc_install_last_v_nginx}"
 			echo "1) ${loc_yes}"
 			echo "2) ${loc_no}"
 			read -p "${loc_choose}: " installnginx
 			if [ $installnginx -eq 1 ]; then
-				echo "${GREEN}${loc_go_to_dir}  /etc/yum.repos.d/${NORMAL}"
+				echo "${loc_go_to_dir}  /etc/yum.repos.d/"
 				cd /etc/yum.repos.d/
-				echo "${GREEN}${loc_remove_repo}${NORMAL}"
+				echo "${loc_remove_repo}"
 				for listnginx in $(grep -rl "nginx.org" /etc/yum.repos.d/)
 				do
 					rm -rf $listnginx
 				done
-				echo "${GREEN}${loc_mainline_repo}${NORMAL}"
+				echo "${loc_mainline_repo}"
 				touch nginx.repo	
 				echo "[nginx]" | tee -a /etc/yum.repos.d/nginx.repo
 				echo "name=nginx repo" | tee -a /etc/yum.repos.d/nginx.repo
 				echo "baseurl=http://nginx.org/packages/mainline/${dist}/${osv}/"'$basearch/' | tee -a /etc/yum.repos.d/nginx.repo
 				echo "gpgcheck=0" | tee -a /etc/yum.repos.d/nginx.repo
 				echo "enabled=1" | tee -a /etc/yum.repos.d/nginx.repo
-				echo "${GREEN}${loc_check_key_sys}${NORMAL}"
+				echo "${loc_check_key_sys}"
 				if [ -z "$(rpm -q gpg-pubkey --qf '%{name}-%{version}-%{release} --> %{summary}\n' | grep nginx)" ]; then
-					echo "${GREEN}${loc_add_key}${NORMAL}"
+					echo "${loc_add_key}"
 					wget http://nginx.org/keys/nginx_signing.key
 					rpm --import nginx_signing.key
 				fi
-				echo "${GREEN}${loc_upgrade_last_ver}${NORMAL}"			
+				echo "${loc_upgrade_last_ver}"			
 				yum update -y nginx
 			else
-				echo "${BOLD}${RED}${loc_no_install_last_ver}${NORMAL}"
+				echo "${loc_no_install_last_ver}"
 			fi
 		else
-			echo "${BOLD}${RED}${loc_setup_last_version}${NORMAL}"
+			echo "${loc_setup_last_version}"
 			if [ -z "$(yum list installed|grep nginx)" ]; then
-				echo "${GREEN}${loc_install_last_ver}${NORMAL}"
+				echo "${loc_install_last_ver}"
 				yum -y update && yum -y install nginx
 			else
-				echo "${GREEN}${loc_upgrade_last_ver}${NORMAL}"
+				echo "${loc_upgrade_last_ver}"
 				yum update -y nginx
 			fi
 		fi
@@ -233,33 +259,33 @@ else
 	checknginxstatus=1
 fi
 if [ $checknginxstatus -eq 1 ]; then
-	echo "${GREEN}${loc_restart_nginx}${NORMAL}"
+	echo "${loc_restart_nginx}"
 	if [ "${dist}" = "debian" ] && [ "${osv}" = "8" ] || [ "${dist}" = "ubuntu" ] && [ "${osv}" = "xenial" ] || [ "${dist}" = "centos" ] && [ "${osv}" = "7" ]; then
 		systemctl restart nginx
-		echo "${GREEN}${loc_status_nginx}${NORMAL}"
+		echo "${loc_status_nginx}"
 		if [ -z "$(systemctl status nginx | grep "inactive")" ] && [ -z "$(systemctl status nginx | grep "failed")" ]; then
 			echo "${loc_upgrade_nginx_sf}!"		
 		else
-			echo "${RED}${BOLD}${loc_upgrade_nginx_faild}${NORMAL}"
-			echo "${BOLD}GitHub https://github.com/alexeymalets/nginx-install-auto/issues${NORMAL}"
-			echo "${BOLD}${loc_srv_admin} http://svradmin.ru/${NORMAL}"
-			echo "${BOLD}${loc_myhosti}(24/7) https://myhosti.pro${NORMAL}"
+			echo "${loc_upgrade_nginx_faild}"
+			echo "GitHub https://github.com/alexeymalets/nginx-install-auto/issues"
+			echo "${loc_srv_admin} http://svradmin.ru/"
+			echo "${loc_myhosti}(24/7) https://myhosti.pro"
 			echo "${loc_error} :$(systemctl status nginx.service
 			)"
 		fi
 	else
 		service nginx restart
-		echo "${GREEN}${loc_status_nginx}${NORMAL}"
+		echo "${loc_status_nginx}"
 		if [ -z "$(service nginx status | grep stopped)" ] && [ -z "$(service nginx status | grep failed)" ]; then
 			echo "${loc_upgrade_nginx_sf}!"		
 		else
-			echo "${RED}${BOLD}${loc_upgrade_nginx_faild}${NORMAL}"
-			echo "${BOLD}GitHub https://github.com/alexeymalets/nginx-install-auto/issues${NORMAL}"
-			echo "${BOLD}${loc_srv_admin} http://svradmin.ru/${NORMAL}"
-			echo "${BOLD}${loc_myhosti}(24/7) https://myhosti.pro${NORMAL}"
+			echo "${loc_upgrade_nginx_faild}"
+			echo "GitHub https://github.com/alexeymalets/nginx-install-auto/issues"
+			echo "${loc_srv_admin} http://svradmin.ru/"
+			echo "${loc_myhosti}(24/7) https://myhosti.pro"
 			echo "${loc_error} :$(tail -n 10 /var/log/nginx/error.log)"
 		fi
 	fi
 fi
 
-echo "${GREEN}${loc_thanks_soft}${NORMAL}"
+echo "${loc_thanks_soft}"
